edd3d510846b4f05c51bf394b05d3605ca5dd78e
Aditya kumar <Adityakumar14022002@gmail.com>
2024-02-29 04:47:24 +0530

    " x = o "

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   output: {
     dir: "dist",
     format: "es",
-    name:"package-aditya.js"
+    name:"aditya.js"
   },
   external: ["react", "react-dom"],
   plugins: [typescript({ tsconfig: "tsconfig.json" })],
